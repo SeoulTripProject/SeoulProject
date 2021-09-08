276db020a342af2459775fed999074f53b26fc4b
KimsFamily <KimsFamily@DESKTOP-QMSTVJM>
2021-09-08 16:08:53 +0900

    1111

---
+++ SeoulProject/src/com/sist/vo/FoodVO.java
@@ -0,0 +1,104 @@
+package com.sist.vo;
+
+public class FoodVO {
+	private int no, rno, good, soso, bad;
+	private double score;
+	private String poster, rname, address, tel, rtype, price, parking, openHour, menu, rtag;
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public int getRno() {
+		return rno;
+	}
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+	public int getGood() {
+		return good;
+	}
+	public void setGood(int good) {
+		this.good = good;
+	}
+	public int getSoso() {
+		return soso;
+	}
+	public void setSoso(int soso) {
+		this.soso = soso;
+	}
+	public int getBad() {
+		return bad;
+	}
+	public void setBad(int bad) {
+		this.bad = bad;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getRname() {
+		return rname;
+	}
+	public void setRname(String rname) {
+		this.rname = rname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getRtype() {
+		return rtype;
+	}
+	public void setRtype(String rtype) {
+		this.rtype = rtype;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getParking() {
+		return parking;
+	}
+	public void setParking(String parking) {
+		this.parking = parking;
+	}
+	public String getOpenHour() {
+		return openHour;
+	}
+	public void setOpenHour(String openHour) {
+		this.openHour = openHour;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public String getRtag() {
+		return rtag;
+	}
+	public void setRtag(String rtag) {
+		this.rtag = rtag;
+	}
+}
